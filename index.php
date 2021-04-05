ae06ed1fd8c34b7340f21f36f4debf2a98269652
emenelka <maha2100@yandex.ru>
2021-04-05 10:18:38 +0400

    add information to mainpage

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="ru">
+ <head>
+  <meta charset="utf-8" />
+  <title>Document</title>
+  <link rel="stylesheet" href="css/style.css">
+ </head>
+ <body>
+   Зазеркалье
+<img src="img/logoMainB.png" alt="Логотип студии">
+   <p>Добро пожаловать в мир чудес и невесомости!</p>
+  <p>Воздушная гимнастика, растяжка и акробатика для всех желающих!</p>
+  <table width="1000" >
+   <tr>
+    <th><img src="img/photo-mp.jpg" width="500" alt="Фото полотна"></th>
+    <td><img src="img/photo-mp-3.jpg" width="500" alt="Фото полотна"></td>
+   </tr>
+ </table>
+
+
+ </body>
+</html>
