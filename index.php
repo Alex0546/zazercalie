a8af7a6cdc75d4dfa653340c40f9e1f56695f3fe
Alex_63 <alex_63@team.mybb.ru>
2021-04-07 11:56:23 +0400

    first commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="ru">
+ <head>
+  <meta charset="utf-8" />
+  <title>Document</title>
+  <link rel="stylesheet" href="css/style.css">
+ </head>
+ <body>
+   <img src="img/logoMainB.png" width="300" alt="Логотип студии">
+   <p>Зазеркалье</p>
+   <p>Добро пожаловать в мир чудес и невесомости!</p>
+   <p>Воздушная гимнастика, растяжка и акробатика для всех желающих!</p>
+  <table width="1000" >
+   <tr>
+    <th><img src="img/photo-mp.jpg" width="500" alt="Фото полотна"></th>
+    <td><img src="img/photo-mp-3.jpg" width="500" alt="Фото кольцо"></td>
+   </tr>
+ </table>
+
+
+ </body>
+</html>
