3a345f26544c10ba1805ac67b3a8a152b131e59b
emenelka <maha2100@yandex.ru>
2021-04-19 12:11:48 +0400

    add description on direction.php and create documents.php

---
+++ trainers.php
@@ -0,0 +1,12 @@
+<?php
+define('SITE', 1);
+define('SITE_ROOT', './');
+require SITE_ROOT . 'includes/header.php';
+?>
+
+  <section>
+  </section>
+
+<?php
+require SITE_ROOT . 'includes/footer.php';
+?>
