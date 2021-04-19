22ef6398b72128c37b759252f2c55cf2a8277134
Alex_63 <alex_63@team.mybb.ru>
2021-04-19 11:34:03 +0400

    fix layout in index.php

--- index.php
+++ index.php
@@ -4,16 +4,16 @@ define('SITE_ROOT', './');
 require SITE_ROOT . 'includes/header.php';
 ?>
 
-  <p>Добро пожаловать в мир чудес и невесомости!</p>
-  <p>Воздушная гимнастика, растяжка и акробатика для всех желающих!</p>
-  <p>Воздушная гимнастика, растяжка и акробатика для всех желающих!</p>
-  <table width="1000" >
-   <tr>
-    <th><img src="img/photo-mp.jpg" width="500" alt="Фото полотна"></th>
-    <td><img src="img/photo-mp-3.jpg" width="500" alt="Фото полотна"></td>
-   </tr>
- </table>
-
+  <section>
+    <p>Добро пожаловать в мир чудес и невесомости!</p>
+    <p>Воздушная гимнастика, растяжка и акробатика для всех желающих!</p>
+    <table width="1000" >
+      <tr>
+        <td><img src="img/photo-mp.jpg" width="500" alt="Фото полотна"></td>
+        <td><img src="img/photo-mp-3.jpg" width="500" alt="Фото полотна"></td>
+      </tr>
+    </table>
+  </section>
 
 <?php
 require SITE_ROOT . 'includes/footer.php';
