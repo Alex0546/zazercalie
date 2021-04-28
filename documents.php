92d8d70b13ecf986f3f091485e0a80c8b193b216
emenelka <maha2100@yandex.ru>
2021-04-28 16:42:46 +0400

    add docs to documents.php - 3

---
+++ documents.php
@@ -0,0 +1,19 @@
+<?php
+define('SITE', 1);
+define('SITE_ROOT', './');
+require SITE_ROOT . 'includes/header.php';
+?>
+
+  <section>
+    <p>
+      <object width="1000" height="1000">
+        <details><summary>Правила посещения студии</summary><embed src="../docs/pravila_pos.pdf" width="1000" height="1000" /></details>
+        <details><summary>Правила пользования абонемента</summary><embed src="../docs/pravila_abons.pdf" width="1000" height="1000" /></details>
+      </object>
+    </p>
+
+  </section>
+
+<?php
+require SITE_ROOT . 'includes/footer.php';
+?>
---
+++ includes/footer.php
@@ -0,0 +1,19 @@
+<?php if (!defined('SITE')) exit; ?>
+
+    <footer>
+        <div>
+            <div class="contacts">
+                +7 (927) 776-59-35<br />
+                Московское шоссе, 34а<br />
+                <a href="https://vk.com/zazercalie_air" class="social-icon"><img src="/img/vk.svg" /></a>
+                <a href="https://instagram.com/zazercalie.air" class="social-icon"><img src="/img/instagram.svg" /></a><br />
+                <a href="mailto:zazercalie.air@yandex.ru">zazercalie.air@yandex.ru</a>
+            </div>
+            <div class="copyright">
+                &#169; <?php echo date('Y');?> Студия воздушного танца &laquo;Зазеркалье&raquo;
+            </div>
+        </div>
+    </footer>
+
+  </body>
+</html>
