432550df35ff9220c47d04964ec062b113b419ab
navi <ivan_23.04.1998@mail.ru>
2021-05-05 16:17:52 +0400

    add page titles

--- includes/common.php
+++ includes/common.php
@@ -1,5 +1,6 @@
 <?php
 header('Content-Type: text/html; charset=utf-8');
 //define('SITE_ROOT', './');
+define('SITE_TITLE', 'Студия воздушного танца');
 
 ?>
--- includes/header.php
+++ includes/header.php
@@ -7,6 +7,7 @@ require SITE_ROOT . 'includes/common.php';
 <!DOCTYPE html>
 <html lang="ru">
     <head>
+        <title><?php echo defined('PAGE_TITLE') ? PAGE_TITLE . " — " . SITE_TITLE : SITE_TITLE ?></title>
         <meta charset="utf-8" />
         <link rel="stylesheet" href="/css/style.css?v=18" />
         <script type="text/javascript" src="https://ajax.​googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
@@ -15,8 +16,8 @@ require SITE_ROOT . 'includes/common.php';
         <header>
             <a href="/">
                 <h1>
-                    <img src="/img/logoMainB.png" width="150" alt="Логотип студии">
-                    Студия воздушного танца
+                    <img src="/img/logoMainB.png" width="150" alt="Логотип студии" />
+                    <?php echo SITE_TITLE . "\n"; ?>
                 </h1>
             </a>
         </header>
