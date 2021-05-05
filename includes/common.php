f695b222633cdd95df1d8998fc922d70e04bd200
navi <ivan_23.04.1998@mail.ru>
2021-05-05 18:35:54 +0400

    add connection to db and output content for directions

--- includes/common.php
+++ includes/common.php
@@ -3,4 +3,15 @@ header('Content-Type: text/html; charset=utf-8');
 //define('SITE_ROOT', './');
 define('SITE_TITLE', 'Студия воздушного танца');
 
+$config_db = [
+    "host" => "localhost",
+    "user" => "f0538536_user",
+    "pass" => "f0538536pass",
+    "name" => "f0538536_db",
+    "port" => "3306"
+];
+
+$link = mysqli_connect($config_db['host'], $config_db['user'], $config_db['pass']) or die('unable to connect. ' . mysqli_connect_error());
+mysqli_select_db($link, $config_db['name']) or die('cannot select db');s
+
 ?>
