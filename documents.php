af1e35334cea32a17f810090c41befa36dd89e7c
emenelka <maha2100@yandex.ru>
2021-05-12 11:12:35 +0400

    modified documents.php

--- documents.php
+++ documents.php
@@ -5,6 +5,48 @@ require SITE_ROOT . 'includes/header.php';
 ?>
 
   <section>
+    <h2>Стоимость занятий</h2>
+    <table width="600" border="1" cellpadding="4" cellspacing="0">
+   <tr>
+    <th colspan="2">Разовое занятие</th>
+   </tr>
+   <tr>
+    <th>Пробное занятие</th><th>200</th>
+   </tr>
+   <tr>
+    <th>Разовое заянтие</th><th>375</th>
+   </tr>
+   <tr>
+    <th>Разовое заянтие (акробатика)</th><th>400</th>
+   </tr>
+   <tr>
+    <th>Индивидуальное заянтие</th><th>1250</th>
+   </tr>
+   <tr>
+    <th colspan="2">Абонементы (30 дней)</th>
+   </tr>
+   <tr>
+    <th>4 занятия</th><th>1400</th>
+   </tr>
+   <tr>
+    <th>8 занятий</th><th>2600</th>
+   </tr>
+   <tr>
+    <th>12 занятий</th><th>3500</th>
+   </tr>
+   <tr>
+    <th>16 занятий</th><th>4400</th>
+   </tr>
+   <tr>
+    <th colspan="2">Абонементы акробатика</th>
+   </tr>
+   <tr>
+    <th>4 занятия (на 45 дней)</th><th>1500</th>
+   </tr>
+   <tr>
+    <th>8 занятий (на 90 дней)</th><th>2800</th>
+   </tr>
+  </table>
     <p>
       <object width="1000" height="1000">
         <details><summary>Правила посещения студии</summary><embed src="../docs/pravila_pos.pdf" width="1000" height="1000" /></details>
